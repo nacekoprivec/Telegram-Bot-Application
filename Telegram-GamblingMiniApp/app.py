c35204bb159c14a61d57187314ffd3acde4119a3
nace <nace.koprivec@gmail.com>
2024-08-18 19:27:05 +0200

    fixes

--- Telegram-GamblingMiniApp/app.py
+++ Telegram-GamblingMiniApp/app.py
@@ -1,100 +1,59 @@
-from flask import Flask, request, jsonify
-import requests
-from config import TELEGRAM_API_TOKEN, parametersGet, url_telegram
-import random
+import os
+from flask import Flask, request, jsonify, render_template
+import stripe
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-class Player:
-    def __init__(self, data):
-        first_name = data["message"]["from"].get("first_name", "")
-        last_name = data["message"]["from"].get("last_name", "")
-        self.full_name = f"{first_name} {last_name}"
-        self.balance = random.randint(1, 10000)
+load_dotenv()  
 
-    def decreaseBalance(self, amount):
-        self.balance -= amount
+stripe.api_key = os.getenv("STRIPE_API_KEY")
 
-players = {}
 
-def get_updates(offset=None):
-    params = parametersGet.copy()
-    if offset:
-        params['offset'] = offset
-    response = requests.get(f'{url_telegram}/getUpdates', params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error fetching updates: {response.status_code}")
-        return {}
 
-def send_message(chat_id, text):
-    params = {
-        'chat_id': chat_id,
-        'text': text
-    }
-    response = requests.post(f'{url_telegram}/sendMessage', params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error sending message: {response.status_code}")
-        return {}
+FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY', '5c349d4aa213fe5703ec3ec125db77e6e2dfe89db9de8539')
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = request.json
+app.config['SECRET_KEY'] = FLASK_SECRET_KEY
 
-    if update.get('message'):
-        chat_id = update['message']['chat']['id']
-        player_id = update['message']['from'].get("id", "")
-        message_text = update['message'].get('text', '')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        response_text = ""
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        amount = request.json.get('amount')
 
-        if message_text.startswith('/'):
-            command = message_text.lower()
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'eur',
+                    'product_data': {
+                        'name': 'Deposit',
+                    },
+                    'unit_amount': amount * 100,
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url='https://gamblingminiapp.vercel.app/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url='https://gamblingminiapp.vercel.app/cancel',
+        )
+        return jsonify(id=session.id)
+    except Exception as e:
+        return jsonify(error=str(e)), 403
 
-            if command == '/start':
-                if player_id not in players:
-                    player = Player(update)
-                    players[player_id] = player
-                response_text = f'Welcome {players[player_id].full_name}! Use /balance to check your balance and /bet to place a bet.'
+@app.route('/success')
+def success():
+    session_id = request.args.get('session_id')
+    return f"Payment succeeded! Session ID: {session_id}"
 
-            elif command == '/balance':
-                if player_id in players:
-                    player = players[player_id]
-                    response_text = f"Your balance is {player.balance:}€."
-                else:
-                    response_text = f"Type /start in order to make an account."
-                
-            elif command == '/bet':
-                if player_id in players:
-                    response_text = f"Place your bet using /bet <amount>."
-                else:
-                    response_text = f"There is no account {update['message']['from'].get('first_name', '')} {update['message']['from'].get('last_name', '')}. Type /start to make one."
-                
-            elif message_text.startswith('/bet '):
-                if player_id in players:
-                    try:
-                        amount = int(message_text.split(' ')[1])
-                        if amount <= 0:
-                            response_text = "Bet amount must be a positive integer."
-                        elif amount > players[player_id].balance:
-                            response_text = "Insufficient balance."
-                        else:
-                            players[player_id].decreaseBalance(amount)
-                            response_text = f"You have successfully bet {amount}€. Your new balance is {players[player_id].balance}€."
-                    except (IndexError, ValueError):
-                        response_text = "Please provide a valid integer amount for the bet."
-                else:
-                    response_text = f"There is no account {update['message']['from'].get('first_name', '')} {update['message']['from'].get('last_name', '')}. Type /start to make one."
+@app.route('/cancel')
+def cancel():
+    return "Payment canceled!"
 
-            else:
-                response_text = "Unknown command. Use /start for help."
 
-            send_message(chat_id, response_text)
-
-    return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
--- Telegram-GamblingMiniApp/config.py
+++
@@ -1,3 +0,0 @@
-TELEGRAM_API_TOKEN = '7452050976:AAFVc8Oaop3sBxSeYBW-FZAjuQ3sIm__uOE'
-parametersGet = {'timeout': 100, 'allowed_updates': ['message']}
-url_telegram = f'https://api.telegram.org/bot{TELEGRAM_API_TOKEN}'
